f0dcaa5af63c231adc7e683f9bf5847d30469ecd
honggosanjaya <honggosanjaya@gmail.com>
2021-06-28 21:16:11 +0700

    feat: fitur filter front-end

---
+++ public/js/filter.js
@@ -0,0 +1,23 @@
+let list = document.querySelectorAll('.list');
+let itemBox = document.querySelectorAll('.itemBox');
+
+for(let i=0; i<list.length; i++ ){
+    list[i].addEventListener('click', function(){
+        for(let j=0; j<list.length; j++){
+            list[j].classList.remove('active');
+        }
+        this.classList.add('active');
+
+        let dataFilter = this.getAttribute('data-filter');
+
+        for(let z=0; z<itemBox.length; z++){
+            itemBox[z].classList.remove('active');
+            itemBox[z].classList.add('hide');
+
+            if(itemBox[z].getAttribute('data-item') == dataFilter || dataFilter == 'all'){
+                itemBox[z].classList.remove('hide');
+                itemBox[z].classList.add('active');
+            }
+        }
+    })
+}
